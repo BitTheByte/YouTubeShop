491993a3de476f2ac4db10707de03e21f9f62038
Ahmed Ezzat <ahmed.ezzat119@outlook.com>
2018-10-06 23:18:22 +0200

    Updated version number

--- YouTubeShop3.py
+++ YouTubeShop3.py
@@ -190,7 +190,7 @@ def show_status(action="",channel_id="",live_count=""):
 >>> ===================================================== <<<
 >>> [DEV] : BitTheByte (Ahmed Ezzat)                      <<<
 >>> [GitHub] : https://www.github.com/bitthebyte          <<<
->>> [Version] : 8.0v                                      <<<
+>>> [Version] : 8.1v                                      <<<
 >>> +++++++++++++++++++++++++++++++++++++++++++++++++++++ <<<
 [#] Editing this banner doesn't make you a programmer :)
 """
